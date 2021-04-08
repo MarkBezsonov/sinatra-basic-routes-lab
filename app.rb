c800e10bb62d3caa004e231d77c901b1b2bc8e06
MarkBezsonov <markbezsonov@gmail.com>
2021-04-07 19:38:06 -0500

    commit tree 1b2110e9c4d3e3ee73d6b82f51f1bb7209f65e59

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Mark"
+    end
+    
+    get '/hometown' do
+        "My hometown is Dallas"
+    end
+    
+    get '/favorite-song' do
+        "My favorite song is 'GTA & Wax Motif - Get It All (Getter and Ray Volpe Remix)'"
+    end
 end
